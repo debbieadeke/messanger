b445f63255fd07a14dcf6163d063ad362d9e237b
debbieadeke <okakadebbie15@gmail.com>
2024-06-12 08:16:21 +0300

    Generating chat layout

--- app/Models/Conversation.php
+++ app/Models/Conversation.php
@@ -31,4 +31,15 @@ class Conversation extends Model
         return $this->belongsTo(User::class, 'user_id2');
      }
 
-}
+     public static function getConversationsForSidebar(User $exceptUser)
+     {
+        $users = User::getUsersExceptUser($exceptUser);
+        $groups = Group::getGroupsForUser($exceptUser);
+        return $users->map (function (User $user) {
+            return $user->toConversationArray();
+        })->concat($groups->map(function (Group $group) {
+            return $group->toConversationArray();
+        }));
+     }
+
+    }
--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -51,4 +52,29 @@ class User extends Authenticatable
     {
         return $this->belongsToMany(Group::class, 'group_users');
     }
-}
+    public static function getConversationsForSidebar(User $exceptUser)
+    {
+        $userId = $exceptUser->id;
+        $query = User::select(['users.*', 'messages.message as last_message','messages.created_at
+        as last_message date'])
+        ->where('users.id', '!=', $userId)
+        ->when(!$exceptUser->is_admin, function ($query) {
+            $query->whereNull('users.blocked_at');
+        })
+        ->leftJoin('conversations', function($join) use ($userId) {
+            $join->on('conversations.user_id1', '=', 'users.id')
+                ->where('conversations.user_id2', '=', $userId)
+                ->orWhere(function ($query) use ($userId) {
+                    $query->on('conversations.user_id2', '=', 'users.id')
+                          ->where('conversations.user_id1', '=', Auth::id());
+                });
+        })
+        ->leftJoin('messages', 'messages.id','=', 'conversations.last_message_id')
+        ->orderByRaw('IFNULL(users.blocked_at,1)')
+        ->orderBy('messages.created_at', 'desc')
+        ->orderBy('users.name')
+        ;
+        //dd($query->toSql());
+        return $query->get();
+    } 
+}       
