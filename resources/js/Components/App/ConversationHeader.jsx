feb38db5d978b00de272f274b448dd92a773ccdc
debbieadeke <okakadebbie15@gmail.com>
2024-06-26 09:06:14 +0300

    🚧 Group CRUD

--- resources/js/Components/App/ConversationHeader.jsx
+++ resources/js/Components/App/ConversationHeader.jsx
@@ -1,9 +1,23 @@
 
 import { Link, usePage } from "@inertiajs/react";
-import { ArrowLeftIcon } from "@heroicons/react/24/solid";
+import { ArrowLeftIcon, PencilSquareIcon } from "@heroicons/react/24/solid";
 import UserAvatar from "./UserAvatar";
 import GroupAvatar from "./GroupAvatar";
 const ConversationHeader = ({selectedConversation}) => {
+
+    const onDeleteGroup = ()=> {
+        if(!window.confirm("Are you sure you want to delete this group")) {
+            return;
+        }
+        axios
+        .delete(route("group.destroy", selectedConversation.id))
+        .then(()=> {
+            console.log(res);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+    };
     return(
         <>
         {selectedConversation && (
@@ -28,8 +42,41 @@ const ConversationHeader = ({selectedConversation}) => {
                         )}
                     </div>
                 </div>
-            </div>
+                {selectedConversation.is_group && (
+                    <div className="flex gap-3">
+                        <GroupDescriptionPopover
+                        description={selectedConversation.description}
+                        />
+                        <GroupUsersPopover
+                        users={selectedConversation.users}
+                        />
+                        {selectedConversation.owner_id == authUser.id && (
+                            <>
+                            <div className="tooltip tooltip-left"
+                            data-tip="Edit Group">
+                                <button onClick={(ev)=>
+                                    emit(
+                                        "GroupModal.show",
+                                        selectedConversation
+                                    )
+                                    }
+                                    className="text-gray-400 hover:text-gray-200">
+                                        <PencilSquareIcon className="w-4"/>
+                                    </button>
+                                    </div>
+                                    <div className="tooltip tooltip-left"
+                                    data-tip="Delete Group">
+                                        <button onClick={onDeleteGroup}
+                                        className="text-gray-400 hover:text-gray-200">
+                                            <TrashIcon className="w-4"/>
+                                        </button>
+                                    </div>
+                                    </>
 
+                                    )}                            
+                    </div>
+                )}
+            </div>
         )}
         </>
     );
---
+++ resources/js/Components/App/GroupModal.jsx
@@ -0,0 +1,17 @@
+import TextAreaInput from "@/Components/TextAreaInput";
+import InputError from "@/Components/InputError";
+import InputLabel from "@Components/InputLabel";
+import Modal from "@/Components/Modal";
+import TextInput from "@/Components/TextInput";
+import SecondaryButton from "@/Components/SecondaryButton";
+import PrimaryButton from "@/Components/PrimaryButton";
+import UserPicker from "@/Components/App/UserPicker";
+import {useForm, usePage } from "@inertiajs/react"
+import { useEventBus } from "@/EventBus";
+import { useEffect, useState } from "react";
+
+export default function GroupModal({show = false, onClose = () => {}}){
+    return(
+
+    );
+}
